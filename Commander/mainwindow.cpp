288198969f600039e74a3bc9afebf8bb025eb8d7
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2018-08-02 21:58:26 +0200

    Version 1.0

---
+++ Commander/mainwindow.cpp
@@ -0,0 +1,59 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QApplication>
+#include <QDebug>
+#include <QShortcut>
+#include <QKeySequence>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    QFont font = QApplication::font();
+    font.setFamily("Hack");
+    font.setPointSize(17);
+    this->greenScheme = false;
+    ui->setupUi(this);
+    this->ui->centralWidget->setTerminalFont(font);
+    this->ui->centralWidget->setScrollBarPosition(QTermWidget::ScrollBarRight);
+    this->ui->centralWidget->setColorScheme("WhiteOnBlack");
+    this->ui->centralWidget->setKeyBindings("linux");
+    connect(this->ui->centralWidget, &QTermWidget::finished, this, &MainWindow::close);
+    QShortcut *copy = new QShortcut(QKeySequence("Ctrl+Shift+C"), this);
+    connect(copy, &QShortcut::activated, this->ui->centralWidget, &QTermWidget::copyClipboard);
+    QShortcut *paste = new QShortcut(QKeySequence("Ctrl+Shift+V"), this);
+    connect(paste, &QShortcut::activated, this->ui->centralWidget, &QTermWidget::pasteClipboard);
+    QShortcut *scheme = new QShortcut(QKeySequence("Ctrl+Shift+S"), this);
+    connect(scheme, &QShortcut::activated, this, &MainWindow::switchScheme);
+    QShortcut *help = new QShortcut(Qt::Key_F1, this);
+    connect(help, &QShortcut::activated, this, &MainWindow::showHelp);
+}
+
+void MainWindow::switchScheme()
+{
+    if (greenScheme)
+    {
+        this->ui->centralWidget->setColorScheme("WhiteOnBlack");
+        greenScheme = false;
+    }
+    else
+    {
+        this->ui->centralWidget->setColorScheme("GreenOnBlack");
+        greenScheme = true;
+    }
+}
+
+void MainWindow::showHelp()
+{
+    QMessageBox *helpbox = new QMessageBox();
+    helpbox->setWindowTitle("Help - Commander");
+    helpbox->setText(tr("Help:\nCtrl+Shift+C: Copy selected text\nCtrl+Shift+V: Paste Clipboard\nCtrl+Shift+S: Switch color scheme\nF1: Show this window"));
+    helpbox->addButton(QMessageBox::Ok);
+    helpbox->show();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
